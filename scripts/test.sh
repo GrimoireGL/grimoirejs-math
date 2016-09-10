b37adabbcaeacc893566e5921be5dd5a4527828e
LimeStreem <LimeStreem@gmail.com>
2016-09-10 18:47:45 +0900

    Enabled test

---
+++ scripts/test.sh
@@ -0,0 +1,8 @@
+npm install typescript@beta
+case $CIRCLE_NODE_INDEX in
+  0)
+    npm run compiler-version
+    npm run build -- --branch=$CIRCLE_BRANCH;;
+  1)
+    npm test -- --branch=$CIRCLE_BRANCH;;
+esac
