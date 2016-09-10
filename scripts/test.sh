af4b71b327ea823a5b62167630c7690c04e4657f
LimeStreem <LimeStreem@gmail.com>
2016-09-10 18:58:34 +0900

    removed checking version command on ci

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,6 @@
 npm install typescript@beta
 case $CIRCLE_NODE_INDEX in
   0)
-    npm run compiler-version
     npm run build -- --branch=$CIRCLE_BRANCH;;
   1)
     npm test -- --branch=$CIRCLE_BRANCH;;
