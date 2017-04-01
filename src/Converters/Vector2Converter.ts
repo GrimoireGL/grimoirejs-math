825a77ccc005c8f57ccc6800f722288ac7b7a9f2
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-04-01 18:12:36 +0900

    fix: converter bugs

--- src/Converters/Vector2Converter.ts
+++ src/Converters/Vector2Converter.ts
@@ -10,6 +10,10 @@ function Vector2Converter(this: Attribute, val: any): any {
     return new Vector2(val, val);
   } else if (Array.isArray(val)) {
     return new Vector2(val[0], val[1]);
+  }else if(val[0] !== void 0 && val[1] !== void 0){
+    return new Vector2(val[0], val[1]);
+  } else if(val.X !== void 0 && val.Y !== void 0){
+    return new Vector2(val.X,val.Y);
   }
 }
 
