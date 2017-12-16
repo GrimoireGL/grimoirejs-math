d8ec349d881c5dab9b9c7a27e6f660b7c3cce78d
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-12-16 18:51:31 +0900

    Adopt core1.0 beta

--- release.sh
+++
@@ -1,6 +0,0 @@
-npm run semantic-release
-if [ $? = "0" ]; then
-  echo PRESET_UPDATE
-  curl "https://preset-updater.herokuapp.com/?repo=preset-basic&repoURL=https://github.com/GrimoireGL/grimoirejs-preset-basic.git&buildNumber=$CIRCLE_BUILD_NUM&currentPkg=grimoirejs-math"
-fi
-exit 0
